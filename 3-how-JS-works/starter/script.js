fc3bab28f9f4cd50a2b3c5973b9b6d152e5e529c
nolbu <nolbu.dev@gmail.com>
2020-02-13 08:33:19 +0900

    Se03.How JavaScript works behind the scenes

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,20 +1,33 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// Hoisting Example 1 : function
+calculateAge(1965);
 
+function calculateAge(year){
+    console.log(2016 - year);
+}
 
+// Hoisting Example 2 : variable
+//retirement(1956);
 
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
 
+// variables
+console.log(age);
+var age = 23;
+console.log(age);
 
+function foo(){
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
 
-
-
-
-
-
-
-
-
+foo();
+console.log(age);
 
 
 ///////////////////////////////////////
@@ -22,27 +35,21 @@
 
 
 // First scoping example
-
-/*
 var a = 'Hello!';
-first();
+first1();
 
-function first() {
+function first1() {
     var b = 'Hi!';
-    second();
+    second1();
 
-    function second() {
+    function second1() {
         var c = 'Hey!';
         console.log(a + b + c);
     }
 }
-*/
-
-
 
 // Example to show the differece between execution stack and scope chain
 
-/*
 var a = 'Hello!';
 first();
 
@@ -58,20 +65,43 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    //console.log(a + b + c + d);
+    console.log(a+d);
+    //second();
 }
-*/
-
-
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(this);
 
+calculateAge(1985);
 
+function calculateAge(year){
+    console.log(2016 - year);
+    console.log(this);
+}
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
 
+        function innerFunnction() {
+            console.log(this);
+        }
+        innerFunnction();
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
